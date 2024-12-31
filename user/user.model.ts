349a9bfd2fefa41304808e33dacd5b6d3b6968ef
zezokamel757 <89239832+zezokamel757@users.noreply.github.com>
2024-12-31 12:01:37 +0200

    user login and regestratiion

---
+++ user/user.model.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+// src/user/user.model.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  name: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop()
+  resetToken: string;
+
+  @Prop()
+  resetTokenExpires: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
